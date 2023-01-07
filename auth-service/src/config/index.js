c4637a36c94e4ea443691d0f2d6dc55a248e7045
Cem <cem.gencer.35@gmail.com>
2023-01-07 18:06:01 +0300

    Config file changed

--- auth-service/src/config/index.js
+++ auth-service/src/config/index.js
@@ -1,29 +1,45 @@
 import { readFileSync, existsSync } from "fs"
 
-export const SESSION_SECRET = existsSync("../run/secrets/auth_session_secret")
-	? readFileSync("../run/secrets/auth_session_secret", "utf-8")
-	: "secret"
-export const DB_URL = existsSync("../run/secrets/auth_db_url")
-	? readFileSync("../run/secrets/auth_db_url", "utf-8")
-	: "auth-db"
-export const DB_PORT = existsSync("../run/secrets/auth_db_port")
-	? parseInt(readFileSync("../run/secrets/auth_db_port", "utf-8"))
-	: 5432
-export const DB_USERNAME = existsSync("../run/secrets/auth_db_username")
-	? readFileSync("../run/secrets/auth_db_username", "utf-8")
-	: "postgres"
-export const DB_PASSWORD = existsSync("../run/secrets/auth_db_password")
-	? readFileSync("../run/secrets/auth_db_password", "utf-8")
-	: "postgres"
-export const DB_NAME = existsSync("../run/secrets/auth_db_name")
-	? readFileSync("../run/secrets/auth_db_name", "utf-8")
-	: "auth"
-export const REDIS_URL = existsSync("../run/secrets/auth_redis_url")
-	? readFileSync("../run/secrets/auth_redis_url", "utf-8")
-	: "redis"
-export const REDIS_PORT = existsSync("../run/secrets/auth_redis_port")
-	? parseInt(readFileSync("../run/secrets/auth_redis_port", "utf-8"))
-	: 6379
-export const MQ_URL = existsSync("../run/secrets/auth_mq_url")
-	? readFileSync("../run/secrets/auth_mq_url", "utf-8")
-	: "amqp://rabbitmq"
+export const SESSION_SECRET = readVariableFromFile(
+	"../run/secrets/auth_session_secret",
+	"secret"
+)
+export const DB_URL = readVariableFromFile(
+	"../run/secrets/auth_db_url",
+	"auth-db"
+)
+export const DB_PORT = parseInt(
+	readVariableFromFile("../run/secrets/auth_db_port", 5432)
+)
+export const DB_USERNAME = readVariableFromFile(
+	"../run/secrets/auth_db_username",
+	"postgres"
+)
+export const DB_PASSWORD = readVariableFromFile(
+	"../run/secrets/auth_db_password",
+	"postgres"
+)
+export const DB_NAME = readVariableFromFile(
+	"../run/secrets/auth_db_name",
+	"auth"
+)
+export const REDIS_URL = readVariableFromFile(
+	"../run/secrets/auth_redis_url",
+	"redis"
+)
+export const REDIS_PORT = parseInt(
+	readVariableFromFile("../run/secrets/auth_redis_port", 6379)
+)
+export const MQ_URL = readVariableFromFile(
+	"../run/secrets/auth_mq_url",
+	"amqp://rabbitmq"
+)
+
+/**
+ * @param {string} path File path
+ * @param {string | number} defaultValue Value to be used if file does not exists
+ * @return {string | number}
+ */
+function readVariableFromFile(path, defaultValue) {
+	return existsSync(filePath) ? readFileSync(filePath, "utf-8") : defaultValue
+}
